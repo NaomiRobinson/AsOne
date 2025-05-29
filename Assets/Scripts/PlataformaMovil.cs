bc4a5fbc85238cdb77de639d7e138887b6e39b5a
NaomiRobinson <128409250+NaomiRobinson@users.noreply.github.com>
2025-05-29 17:09:35 -0300

    NuevosNiveles

--- Assets/Scripts/PlataformaMovil.cs
+++ Assets/Scripts/PlataformaMovil.cs
@@ -7,7 +7,7 @@ public class PlataformaMovil : MonoBehaviour
     [SerializeField] private float velocidad;
     private int siguientePlataforma = 1;
     private bool ordenPlataformas = true;
-  
+
     private void Update()
     {
         if (ordenPlataformas && siguientePlataforma + 1 >= puntosMovimiento.Length)
@@ -33,31 +33,30 @@ public class PlataformaMovil : MonoBehaviour
         }
         transform.position = Vector2.MoveTowards(transform.position, puntosMovimiento[siguientePlataforma].position, velocidad * Time.deltaTime);
     }
-
-   private void OnCollisionEnter2D(Collision2D other)
-{
-    if (other.gameObject.CompareTag("JugadorIzq") || other.gameObject.CompareTag("JugadorDer"))
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
-        foreach (ContactPoint2D contacto in other.contacts)
+        if (other.gameObject.CompareTag("JugadorIzq") || other.gameObject.CompareTag("JugadorDer"))
         {
-            if (contacto.normal.y > 0.5f) // El jugador toca desde abajo
+            foreach (ContactPoint2D contacto in other.contacts)
             {
-                Debug.Log("El jugador murió al tocar la plataforma desde abajo");
+                // Convertir la normal del contacto al espacio local de la plataforma
+                Vector2 normalLocal = transform.InverseTransformDirection(contacto.normal);
 
-                  other.gameObject.GetComponent<ReiniciarNivel>().Reiniciar();  
-          
-                other.transform.SetParent(null);
-                break;
-            }
-            else if (contacto.normal.y < -0.5f) 
-            {
-                other.transform.SetParent(this.transform);
-                break;
+                if (normalLocal.y > 0.5f) // El jugador toca desde ARRIBA
+                {
+                    Debug.Log("El jugador murió al tocar la plataforma desde arriba");
+                    other.gameObject.GetComponent<ReiniciarNivel>().Reiniciar();
+                    other.transform.SetParent(null);
+                    break;
+                }
+                else if (normalLocal.y < -0.5f) // El jugador toca desde ABAJO (como si caminara en el techo)
+                {
+                    other.transform.SetParent(this.transform);
+                    break;
+                }
             }
         }
     }
-}
 
 
     private void OnCollisionExit2D(Collision2D other)
